fd1a62788b71047afa7e8e4468d3655e46fedfe4
Glib Briia <glib.briia@gmail.com>
2018-10-24 17:27:17 +0100

    Add glue

--- examples/src/test/scala/cucumber/examples/scalacalculator/RunCukesTest.scala
+++ examples/src/test/scala/cucumber/examples/scalacalculator/RunCukesTest.scala
@@ -1,7 +1,9 @@
 package cucumber.examples.scalacalculator
 
+import cucumber.api.CucumberOptions
 import org.junit.runner.RunWith
 import cucumber.api.junit.Cucumber
 
 @RunWith(classOf[Cucumber])
+@CucumberOptions(glue = Array("src/test/scala/cucumber/examples/scalacalculator"))
 class RunCukesTest
