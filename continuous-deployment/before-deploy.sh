c48a567db4e01598241ece77db7f2eab6153a025
M.P. Korstanje <rien.korstanje@gmail.com>
2019-08-25 19:45:42 +0200

    [Build] Remove automated release steps

--- continuous-deployment/before-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_1fba5115ab15_key -iv $encrypted_1fba5115ab15_iv -in continuous-deployment/codesigning.asc.enc -out continuous-deployment/codesigning.asc -d
-    gpg -q --fast-import continuous-deployment/codesigning.asc
-fi
